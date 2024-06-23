d9491d56328441fc55cec48ce6be8f28da5d3cfc
mahfuz <mahfuzsarker546@gmail.com>
2024-06-23 17:52:16 +0600

    change

--- laravel-file-upload/app/Http/Controllers/Controller.php
+++ laravel-file-upload/app/Http/Controllers/Controller.php
@@ -2,7 +2,36 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class ProfileController extphpends Controller
 {
-    //
+    public function index($id)
+    {
+        // Declare variables and assign the values stated in the question
+        $name = "Donal Trump";
+        $age = "75";
+
+        // Add three variables in $data variable as associative array with values stated in the question
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+        // Set your cookie variables stated in the question
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $minutes = 1;
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        // Create the response with the data, status code, and set the cookie
+        return response()
+            ->json($data, 200)
+            ->cookie($cookieName, $cookieValue, $minutes, $path, $domain, $secure, $httpOnly);
+    }
 }
